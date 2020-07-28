2e2161e788c5cb7d12749dcca5c3248875416195
Chun-Fu Chao <aaefiikmnnnr@gmail.com>
2020-07-28 11:44:35 +0800

    Fourth test

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -2,9 +2,19 @@ class Bottles
     def verse(lineNumber)
         bottlesOnTheWall = lineNumber;
         bottlesRemainOnTheWall = lineNumber - 1; 
-        return bottlesOnTheWall.to_s + " bottles of beer on the wall, " +
-               bottlesOnTheWall.to_s + " bottles of beer.\n" +
-               "Take one down and pass it around, " +
-               bottlesRemainOnTheWall.to_s + (bottlesRemainOnTheWall == 1 ? " bottle" : " bottles") + " of beer on the wall.\n";
+        return printBottle(bottlesOnTheWall) + " of beer on the wall, " +
+               printBottle(bottlesOnTheWall) + " of beer.\n" +
+               "Take " + (bottlesOnTheWall != 1 ? "one" : "it") + " down and pass it around, " +
+               printBottle(bottlesRemainOnTheWall).downcase + " of beer on the wall.\n";
+    end
+
+    def printBottle(count)
+        if 1 < count
+            return count.to_s + " bottles"
+        elsif count == 1
+            return "1 bottle"
+        else
+            return "No more bottles"
+        end
     end
 end
