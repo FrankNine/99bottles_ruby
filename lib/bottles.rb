79b4e0b03253702ca1adc45b980a877ca4afbab2
Chun-Fu Chao <aaefiikmnnnr@gmail.com>
2020-07-28 11:27:22 +0800

    Second test

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,8 +1,8 @@
 class Bottles
     def verse(lineNumber)
-        return "99 bottles of beer on the wall, " +
-      "99 bottles of beer.\n" +
+        return lineNumber.to_s + " bottles of beer on the wall, " +
+      lineNumber.to_s + " bottles of beer.\n" +
       "Take one down and pass it around, " +
-      "98 bottles of beer on the wall.\n";
+      (lineNumber - 1).to_s + " bottles of beer on the wall.\n";
     end
 end
